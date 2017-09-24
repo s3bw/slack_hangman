90531829a819de358ca906ae722255555979f46e
Sebastien (Work) <sebastien@cytora.com>
2017-09-24 01:31:01 +0100

    Initialise bot template responses
    including new templates

---
+++ bot_utils/response_templates.py
@@ -0,0 +1,21 @@
+
+STATS_TEMPLATE = """`Game Play Stats:`
+High Score: `{0[0]}`
+Deaths: `{0[1]}`
+Words Seen: `{0[2]}`
+Current Streak: `{0[3]}`
+"""
+
+DEATH_TEMPLATE = """You have died... the word was *{0[0]}*. {0[1]}
+How would you like to continue?
+Display Stats: `?stats`
+Leaderboard: `?leaders`
+Your stats: `?me`
+Play Again: `?hangman`
+"""
+
+VICTORY_TEMPLATE = """Great Work!
+{}
+"""
+
+GAME_STEP = '{} Hidden word: `{}`'
